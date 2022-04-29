5a34db7534af06d504fbf6cec28d7a345059462b
krouspy <krouspyy@gmail.com>
2022-04-29 13:46:13 +0200

    feat: add core package

---
+++ packages/core/.eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@starkyproject/config/eslint-preset');
---
+++ packages/core/src/index.ts
@@ -0,0 +1,6 @@
+export * from '@starkyproject/account';
+export * from '@starkyproject/contract';
+export * from '@starkyproject/key-pair';
+export * from '@starkyproject/providers';
+export * from '@starkyproject/selector';
+export * from '@starkyproject/utils';
